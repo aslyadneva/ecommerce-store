55133849a8bfe002495c722964dca37b1304574a
Anastasia <slyadneva.anastasia@gmail.com>
2020-05-26 16:52:44 -0500

    Updates

---
+++ precache-manifest.0a012bd9e79efbd2fa179d2b28bb721d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23df4a7b267cdc907fd558304ee3a43f",
+    "url": "/ecommerce-store/index.html"
+  },
+  {
+    "revision": "af465de7d121fea64251",
+    "url": "/ecommerce-store/static/css/main.aba6f494.chunk.css"
+  },
+  {
+    "revision": "32c78283a5eb976112e8",
+    "url": "/ecommerce-store/static/js/2.f80dd1c7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ecommerce-store/static/js/2.f80dd1c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af465de7d121fea64251",
+    "url": "/ecommerce-store/static/js/main.9926869d.chunk.js"
+  },
+  {
+    "revision": "5e80d4de94aabcffd793",
+    "url": "/ecommerce-store/static/js/runtime-main.00b60670.js"
+  },
+  {
+    "revision": "3b18fea76345b5074e8e0f9a4ed071a3",
+    "url": "/ecommerce-store/static/media/blackjeans.3b18fea7.jpeg"
+  },
+  {
+    "revision": "e759e1df85141b3238662564795d39b1",
+    "url": "/ecommerce-store/static/media/bluejeans.e759e1df.jpeg"
+  },
+  {
+    "revision": "2e5497ed19afaadd7656f7b7f3934d97",
+    "url": "/ecommerce-store/static/media/creamjeans.2e5497ed.jpeg"
+  },
+  {
+    "revision": "a9f9a1591c972de52a77c858f7bccad0",
+    "url": "/ecommerce-store/static/media/free-shipping.a9f9a159.gif"
+  },
+  {
+    "revision": "d202cf034a781f65f38779c607358e8c",
+    "url": "/ecommerce-store/static/media/greyjeans.d202cf03.jpeg"
+  },
+  {
+    "revision": "260907c50b1a84fba2f20f49e2d56e5b",
+    "url": "/ecommerce-store/static/media/lightbluejeans.260907c5.jpeg"
+  },
+  {
+    "revision": "38440987b2e83c0f8b7d4abc5e4828df",
+    "url": "/ecommerce-store/static/media/list-header.38440987.jpg"
+  },
+  {
+    "revision": "f2d6f55fe1dd8d8e528a950f97f059cd",
+    "url": "/ecommerce-store/static/media/mainvideo2.f2d6f55f.mp4"
+  },
+  {
+    "revision": "d720c2c6bcf4c9594f69f596e954a4e9",
+    "url": "/ecommerce-store/static/media/photo1.d720c2c6.jpg"
+  },
+  {
+    "revision": "5a844a814020908443d855932dd230de",
+    "url": "/ecommerce-store/static/media/photo2.5a844a81.jpg"
+  },
+  {
+    "revision": "76ad63684e449182646fd45a5fb570b9",
+    "url": "/ecommerce-store/static/media/photo4.76ad6368.jpg"
+  },
+  {
+    "revision": "3eb57b56a0f9a5c4463dba27068b2db1",
+    "url": "/ecommerce-store/static/media/pinkjeans.3eb57b56.jpeg"
+  },
+  {
+    "revision": "83f7175ee526df3a2e3af9a7356bfe17",
+    "url": "/ecommerce-store/static/media/productListHeader.83f7175e.jpg"
+  },
+  {
+    "revision": "cb1c9b62bfb84c6e26cdcfd40e302dd7",
+    "url": "/ecommerce-store/static/media/stonejeans.cb1c9b62.jpeg"
+  },
+  {
+    "revision": "b9a7b22a2d85fba49ad95a733829f753",
+    "url": "/ecommerce-store/static/media/whitejeans.b9a7b22a.jpeg"
+  }
+]);
