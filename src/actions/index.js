298670c0f05b3bbd19d1905a288a430832bf59ae
Anastasia <slyadneva.anastasia@gmail.com>
2020-07-09 15:23:12 -0500

    Firebase update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,256 +1,255 @@
-import { 
+import {
   CHANGE_AUTH,
-  OPEN_CART, CLOSE_CART, ADD_TO_CART, REMOVE_FROM_CART,
-  DECREASE_QUANTITY, INCREASE_QUANTITY, 
-  OPEN_SIDENAV, CLOSE_SIDENAV, 
-  OPEN_SORT, CLOSE_SORT, SORT, GET_SORT_DISTANCE, 
-  SET_NAV, 
-  CHECKING_OUT, 
-  UPDATE_SUBTOTAL, 
-  ADD_SHIPPING, 
-  CLEAR_CART, CLEAR_TOTAL, CLEAR_SORT, GET_PRODUCTS} from './types'; 
+  OPEN_CART,
+  CLOSE_CART,
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  DECREASE_QUANTITY,
+  INCREASE_QUANTITY,
+  OPEN_SIDENAV,
+  CLOSE_SIDENAV,
+  OPEN_SORT,
+  CLOSE_SORT,
+  SORT,
+  GET_SORT_DISTANCE,
+  SET_NAV,
+  CHECKING_OUT,
+  UPDATE_SUBTOTAL,
+  ADD_SHIPPING,
+  CLEAR_CART,
+  CLEAR_TOTAL,
+  CLEAR_SORT,
+  GET_PRODUCTS,
+} from "./types";
 
-import googleAuthApi from '../apis/googleAuth'; 
-import { firestore } from '../firebase';
+import googleAuthApi from "../apis/googleAuth";
+import { firestore } from "../firebase";
 
-
-const googleAuthApiInstance = new googleAuthApi();  
-
+const googleAuthApiInstance = new googleAuthApi();
 
 export const initAuth = () => {
-
-  return function (dispatch) {    
+  return function (dispatch) {
     try {
       googleAuthApiInstance.init(dispatch);
     } catch (err) {
       console.log(err);
     }
-  }
-}
+  };
+};
 
 export const changeAuth = () => {
-  let currentUser; 
-  const isSignedIn = googleAuthApiInstance.isSignedIn(); // returns either true or false 
+  let currentUser;
+  const isSignedIn = googleAuthApiInstance.isSignedIn(); // returns either true or false
 
   if (isSignedIn) {
     try {
-      currentUser = googleAuthApiInstance.getCurrentUser()
+      currentUser = googleAuthApiInstance.getCurrentUser();
     } catch (err) {
       console.log(err);
     }
-    
   }
 
   return function (dispatch) {
     dispatch({
-      type: CHANGE_AUTH, 
-      payload: {signedIn: isSignedIn, user: currentUser}
-    })
-  }  
-}
+      type: CHANGE_AUTH,
+      payload: { signedIn: isSignedIn, user: currentUser },
+    });
+  };
+};
 
-export const trySignIn = () => {   
+export const trySignIn = () => {
   return function () {
     try {
       googleAuthApiInstance.signIn();
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 };
 
 export const trySignOut = () => {
   return function () {
     try {
-      googleAuthApiInstance.signOut(); 
+      googleAuthApiInstance.signOut();
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 };
 
 export const getProducts = () => {
   return function (dispatch) {
-
-    firestore.collection('products').get()
-      .then(items => items.docs.map(item => {
-        let itemObj = {
-          id: item.id, 
-          ...item.data()
-        }
-        return itemObj
-      }))
-      .then(itemsArr => dispatch({ type: GET_PRODUCTS, payload: itemsArr }))
-  }
-}
-
+    firestore
+      .collection("products")
+      .get()
+      .then((items) => {
+        // console.log(items);
+        return items.docs.map((item) => {
+          let itemObj = {
+            id: item.id,
+            ...item.data(),
+          };
+          return itemObj;
+        });
+      })
+      .then((itemsArr) => dispatch({ type: GET_PRODUCTS, payload: itemsArr }))
+      .catch((err) => console.log("This is the error", err));
+  };
+};
 
 export const openCart = () => {
   return {
-    type: OPEN_CART
-  }; 
-}; 
+    type: OPEN_CART,
+  };
+};
 
 export const closeCart = () => {
   return {
-    type: CLOSE_CART
-  }; 
+    type: CLOSE_CART,
+  };
 };
 
 export const clearCart = () => {
   return {
-    type: CLEAR_CART
-  }; 
+    type: CLEAR_CART,
+  };
 };
 
 export const addShipping = (type) => {
   return {
-    type: ADD_SHIPPING, 
-    payload: type
-  }
-}
+    type: ADD_SHIPPING,
+    payload: type,
+  };
+};
 
 export const clearTotal = () => {
   return {
-    type: CLEAR_TOTAL
-  }
-}
+    type: CLEAR_TOTAL,
+  };
+};
 
 export const addToCart = (product) => {
-
-  return function (dispatch, getState){
-    const { cart } = getState(); 
+  return function (dispatch, getState) {
+    const { cart } = getState();
 
     dispatch({
-      type: ADD_TO_CART, 
-      payload: {product, cartItems: cart.products}
-    })
-    
-    dispatch({
-      type: UPDATE_SUBTOTAL, 
-      payload: getState().cart.products
-    })
-  }
+      type: ADD_TO_CART,
+      payload: { product, cartItems: cart.products },
+    });
 
+    dispatch({
+      type: UPDATE_SUBTOTAL,
+      payload: getState().cart.products,
+    });
+  };
 };
 
 export const removeFromCart = (product) => {
-  return function (dispatch, getState){
+  return function (dispatch, getState) {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      payload: product,
+    });
 
     dispatch({
-      type: REMOVE_FROM_CART,  
-      payload: product
-    })
-    
-    dispatch({
-      type: UPDATE_SUBTOTAL, 
-      payload: getState().cart.products
-    })
-
-  }; 
+      type: UPDATE_SUBTOTAL,
+      payload: getState().cart.products,
+    });
+  };
 };
 
 export const increaseQuantity = (product) => {
-
   return function (dispatch, getState) {
-
     const { cart } = getState();
 
     dispatch({
-      type: INCREASE_QUANTITY,  
-      payload:  {productToIncrease: product, cartItems: cart.products }
-    })
+      type: INCREASE_QUANTITY,
+      payload: { productToIncrease: product, cartItems: cart.products },
+    });
 
     dispatch({
-      type: UPDATE_SUBTOTAL, 
-      payload: getState().cart.products
-    })
-  }
-
+      type: UPDATE_SUBTOTAL,
+      payload: getState().cart.products,
+    });
+  };
 };
 
 export const decreaseQuantity = (product) => {
-
   return function (dispatch, getState) {
-
     const { cart } = getState();
 
     dispatch({
-      type: DECREASE_QUANTITY,  
-      payload:  {productToDecrease: product, cartItems: cart.products }
-    })
+      type: DECREASE_QUANTITY,
+      payload: { productToDecrease: product, cartItems: cart.products },
+    });
 
     dispatch({
-      type: UPDATE_SUBTOTAL, 
-      payload: getState().cart.products
-    })
-  }
-  
+      type: UPDATE_SUBTOTAL,
+      payload: getState().cart.products,
+    });
+  };
 };
 
 export const openSideNav = () => {
   return {
-    type: OPEN_SIDENAV
-  }; 
-}; 
+    type: OPEN_SIDENAV,
+  };
+};
 
 export const closeSideNav = () => {
   return {
-    type: CLOSE_SIDENAV
-  }; 
-}; 
+    type: CLOSE_SIDENAV,
+  };
+};
 
 export const openSort = () => {
   return {
-    type: OPEN_SORT
-  }; 
-};  
+    type: OPEN_SORT,
+  };
+};
 
 export const closeSort = () => {
   return {
-    type: CLOSE_SORT
-  }; 
-}; 
+    type: CLOSE_SORT,
+  };
+};
 
 export const sort = (sortType) => {
-
   return function (dispatch, getState) {
-
-    const { firestoreProducts } = getState(); 
+    const { firestoreProducts } = getState();
 
     dispatch({
-      type: SORT, 
-      payload: { sortType: sortType, products: firestoreProducts }
+      type: SORT,
+      payload: { sortType: sortType, products: firestoreProducts },
     });
-    
-    dispatch(closeSort())
-  }
-}; 
+
+    dispatch(closeSort());
+  };
+};
 
 export const clearSort = () => {
   return {
-    type: CLEAR_SORT
-  }; 
-}; 
+    type: CLEAR_SORT,
+  };
+};
 
 export const getSortDistance = (distance) => {
   return {
     type: GET_SORT_DISTANCE,
-    payload: distance
-  }; 
-}; 
+    payload: distance,
+  };
+};
 
 export const setNavColor = (path) => {
   return {
     type: SET_NAV,
-    payload: path
-  }; 
-}; 
+    payload: path,
+  };
+};
 
 export const checkingOut = (value) => {
   return {
-    type: CHECKING_OUT, 
-    payload: value
-  }; 
-}; 
- 
+    type: CHECKING_OUT,
+    payload: value,
+  };
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,104 +1,126 @@
-import React, { Component, Fragment, Suspense } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom'; 
+import React, { Component, Fragment, Suspense } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-import { connect } from 'react-redux'; 
-import { initAuth, getProducts, closeSideNav } from '../actions'; 
+import { connect } from "react-redux";
+import { initAuth, getProducts, closeSideNav } from "../actions";
 
-import Navigation from './Navigation'; 
-import Newsletter from './Newsletter'; 
-import Footer from './Footer';
-import Home from './pages/Home/Home'; 
+import Navigation from "./Navigation";
+import Newsletter from "./Newsletter";
+import Footer from "./Footer";
+import Home from "./pages/Home/Home";
 
-import Login from './pages/Login';
-import Account from './pages/Account';
-import Cart from './cart/Cart'; 
-import SideNav from './SideNav'; 
-import Modal from './Modal'; 
-import Checkout from './pages/Checkout/Checkout'; 
-import Search from './Search';
-import Loading from './Loading';
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import Cart from "./cart/Cart";
+import SideNav from "./SideNav";
+import Modal from "./Modal";
+import Checkout from "./pages/Checkout/Checkout";
+import Search from "./Search";
+import Loading from "./Loading";
 
-
-const LazyProductList = React.lazy(() => import('./pages/ProductList/ProductList'));
-const LazyProduct = React.lazy(() => import('../components/pages/Product/Product'));
-
-
-
+const LazyProductList = React.lazy(() =>
+  import("./pages/ProductList/ProductList")
+);
+const LazyProduct = React.lazy(() =>
+  import("../components/pages/Product/Product")
+);
 
 class App extends Component {
   state = {
-    searchOpen: false
-  }
+    searchOpen: false,
+  };
 
-  componentDidMount ()  {
+  componentDidMount() {
     // this.props.initAuth();
     this.props.getProducts();
-  }  
+  }
 
   renderPrivateRoute = () => {
-    if (this.props.signedIn === 'pending') {
-      return <div>Loading...</div>
+    if (this.props.signedIn === "pending") {
+      return <div>Loading...</div>;
     } else if (this.props.signedIn) {
-      return <Account/>
+      return <Account />;
     } else {
-      return <Redirect to="/login"/>
+      return <Redirect to="/login" />;
     }
-  }
+  };
 
   openSearch = () => {
     if (this.props.sideNav) {
-      this.props.closeSideNav()
+      this.props.closeSideNav();
     }
-    this.setState({searchOpen : true})
-  }
- 
+    this.setState({ searchOpen: true });
+  };
+
   closeSearch = () => {
-    this.setState({searchOpen : false})
-  }
+    this.setState({ searchOpen: false });
+  };
 
-  render () {
-    const {searchOpen} = this.state
-    const {signedIn, checkingOut} = this.props
+  render() {
+    const { searchOpen } = this.state;
+    const { signedIn, checkingOut } = this.props;
 
-    return (   
-      <Fragment> 
-          {searchOpen && <Search close={this.closeSearch}/>}
-          {!searchOpen &&  <SideNav openSearch={this.openSearch}/>} 
-          <Route path="/" component={Cart}/>
-          <Modal/>
-              {this.props.checkingOut ? null : <Navigation openSearch={this.openSearch}/>}                    
-              <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/account" exact>
-                  {this.renderPrivateRoute()}
-                </Route>               
-                <Route path="/login" exact> 
-                  {signedIn ? <Redirect to="/account"/> : <Login/>}
-                </Route>      
+    return (
+      <Fragment>
+        {searchOpen && <Search close={this.closeSearch} />}
+        {!searchOpen && <SideNav openSearch={this.openSearch} />}
+        <Route path="/" component={Cart} />
+        <Modal />
+        {this.props.checkingOut ? null : (
+          <Navigation openSearch={this.openSearch} />
+        )}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/account" exact>
+            {this.renderPrivateRoute()}
+          </Route>
+          <Route path="/login" exact>
+            {signedIn ? <Redirect to="/account" /> : <Login />}
+          </Route>
 
-                <Route path="/products" exact render={(props) => <Suspense fallback={<Loading />}><LazyProductList {...props}/></Suspense> }/>
+          <Route
+            path="/products"
+            exact
+            render={(props) => (
+              <Suspense fallback={<Loading />}>
+                <LazyProductList {...props} />
+              </Suspense>
+            )}
+          />
 
-                <Route path="/products/:name" exact render={(props) => <Suspense fallback={<Loading/>}><LazyProduct {...props}/></Suspense> }/>
+          <Route
+            path="/products/:name"
+            exact
+            render={(props) => (
+              <Suspense fallback={<Loading />}>
+                <LazyProduct {...props} />
+              </Suspense>
+            )}
+          />
 
-                <Route path="/checkout" exact>
-                  {checkingOut ? <Checkout/> : <Redirect to="/"/>}
-                </Route>
-              </Switch>
-              {this.props.checkingOut ? null :  <Newsletter />}  
-              {this.props.checkingOut ? null :  <Footer/>}                
-      </Fragment> 
+          <Route path="/checkout" exact>
+            {checkingOut ? <Checkout /> : <Redirect to="/" />}
+          </Route>
+        </Switch>
+        {this.props.checkingOut ? null : <Newsletter />}
+        {this.props.checkingOut ? null : <Footer />}
+      </Fragment>
     );
-  }  
+  }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     sortTab: state.sort.sortTab,
-    cartOpen: state.cart.isOpen, 
-    checkingOut: state.checkOut.inProgress, 
-    signedIn: state.auth.isSignedIn, 
-    sideNav: state.sideNav
-  }
-}
+    cartOpen: state.cart.isOpen,
+    checkingOut: state.checkOut.inProgress,
+    signedIn: state.auth.isSignedIn,
+    sideNav: state.sideNav,
+  };
+};
 
-export default connect(mapStateToProps, { initAuth, getProducts, closeSideNav })(App);
+export default connect(mapStateToProps, {
+  initAuth,
+  getProducts,
+  closeSideNav,
+})(App);
